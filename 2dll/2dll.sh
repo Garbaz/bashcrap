ecc2212cc84bb9eb29ce258b2289d9035bde1d8a
Garbaz <garbaz@t-online.de>
2015-11-17 10:28:21 +0100

    updated 17.11.15

--- 2dll/2dll.sh
+++ 2dll/2dll.sh
@@ -5,7 +5,8 @@
 # Give a file location as the first argument for the script.            #
 # The file should only contain the frames, with an empty line between   #
 # each frame.                                                           #
-# To print an empty line in a frame use  '\ ' (With the space!).        #
+# To print an empty line in a frame, just put a space in there, so that #
+# it only looks empty.                                                  #
 # The script won't clear the screen after each frame, so make sure      #
 # every frame has the same character width and height (Use spaces if    #
 # required).                                                            #
@@ -20,49 +21,62 @@
 # Script by: Garbaz (garbaz@t-online.de)                                #
 #########################################################################
 
+#################################
+# TODO:                         #
+# - Enable easier use of spaces #
+#                               #
+#################################
+
 #!/bin/bash
 
-noloop="nope"
+IFS='' #Do not remove spaces!
 
-trap ctrl_c INT
+noloop="nope" #When the second argument is equal this string, do not loop
 
-function ctrl_c() {
+trap ctrl_c INT # Trap interrupting to make the cursor visible again
+
+function ctrl_c() { 
+ IFS=' '
  tput cvvis
  exit 0
 }
 
+#Check whether the first argument was given. Write warning and terminate if not
 if [[ -z $1 ]];then
 printf "\nPlease provide the file:\n"
 printf "\"./2dll.sh FILE [LOOP] [TIME]\"\n\n"
 exit 1
 fi
 
+#Check whether the third argument was given. Set the T to 0.5 if not
 if [[ -z $3 ]];then
-t="0.5"
+T="0.5"
 else
-t=$3
+T=$3
 fi
 
-file=`cat $1`
-
+#Read file and handle backslashes ("Prevent" handeling '\' as an escape char)
+file=$(cat $1 | sed 's/\\/\\\\\\\\/g')
 tput clear
 tput civis
 
 while true; do
 l=0
 while read line; do
-tput cup $l 0
-l=$(($l + 1))
-if [[ -z $line ]]; then
-l=0
-sleep $t
+tput cup $l 0 #Set cursor position
+l=$(($l + 1)) # Increment line counter
+if [[ -z $line ]]; then # Check for empty line
+l=0 # Reset line counter. New frame will overwrite old one
+sleep $T #Display frame for T seconds
 else
-printf "$line"
+printf "$line" # Print line (if not empty)
 fi
-done <<< "$file"
-sleep $t
+done <<< "$file" # piping the file string into the read
+sleep $T
 if [[ "$2" == "$noloop" ]]; then
 printf "\n"
-break
+break # Exit if looping were disabled
 fi
 done
+IFS=' '
+tput cvvis
